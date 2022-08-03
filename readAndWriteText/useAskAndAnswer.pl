89e4ed299576235fa9ff030b7051040877c674b6
gabarnettjr <gabarnettjr@gmail.com>
2022-08-03 11:45:47 -0600

    Create useAskAndAnswer.pl

---
+++ readAndWriteText/useAskAndAnswer.pl
@@ -0,0 +1,16 @@
+#!/mu/bin/perl
+
+use strict;
+
+use Math::Matrix;
+
+system "./askPositiveOrNegative.pl";
+
+system "./answerPositiveOrNegative.pl";
+
+# my $A = Math::Matrix -> new([rand, rand, rand],
+#                             [rand, rand, rand],
+#                             [rand, rand, rand]);
+# 
+# $A -> print("A\n");
+
