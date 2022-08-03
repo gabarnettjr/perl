193bef5e191d7f8535e22351fc9d545b34f6cb11
gabarnettjr <gabarnettjr@gmail.com>
2022-08-03 11:42:32 -0600

    Create brackets_or_parens.pl

---
+++ brackets_or_parens.pl
@@ -0,0 +1,15 @@
+#!/mu/bin/perl
+
+use strict;
+
+my @x = ("hello", "there");
+
+my @y = ["hello", "there"];
+
+print("\n\@x = @x,    \@y = @y\n\n");
+
+print("\nWhat do the square brackets do?");
+print("\nSomething to do with a reference to an array?");
+
+print("\n\n");
+
