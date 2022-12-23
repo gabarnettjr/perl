9dfcf260e30da4616d7c6f864f37dbfd038c4f63
gabarnettjr <gabarnettjr@gmail.com>
2022-12-23 09:24:06 -0700

    Update Hand.pm

--- playingCards/Hand.pm
+++ playingCards/Hand.pm
@@ -13,9 +13,33 @@ sub new {
 
     my @hand = ();
     for (my $i = 1; $i <= $cardsPerHand; $i++) {
-        push @hand, (shift @{$deck});
+        push(@hand, (shift @{$deck}));
     }
     return @hand;
+}
+
+sub contains {
+    # Determine if a hand contains a specific card.
+    my ($hand, $card) = @_;
+
+    foreach my $c (@{$hand}) {
+        if (Card::sameCard($c, $card)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+sub containsFace {
+    # Determine if a hand contains a card with a specific face.
+    my ($hand, $face) = @_;
+
+    foreach my $c (@{$hand}) {
+        if (@{$c}[0] eq $face) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 sub toString {
@@ -23,10 +47,17 @@ sub toString {
 
     my $s = "\n";
     foreach my $card (@{$hand}) {
-        $s .=  (Card::toString $card);
+        $s .=  Card::toString($card);
     }
     return $s;
 }
 
 return 1;
+
+
+
+
+
+
+
 
