ac8a7aaa067a812da1ae167783331d187f39a643
gabarnettjr <gabarnettjr@gmail.com>
2023-08-03 18:36:41 -0600

    Update RowVector.pm

--- LinearAlgebra/RowVector.pm
+++ LinearAlgebra/RowVector.pm
@@ -7,6 +7,15 @@ sub new {
     my $numElements = undef;
     my $elements = undef;
     my $self = [$numElements, $elements];
-    bless($self);
-    return($self);
+    bless $self;
+    return $self ;
+}
+
+sub numElements {
+    my $self = shift;
+    my $numElements = @{$self}[0];
+    return $numElements if defined $numElements;
+    my $elements = $self->elements();
+    return scalar @{$elements};
+    die "Failed to get the number of elements.";
 }
