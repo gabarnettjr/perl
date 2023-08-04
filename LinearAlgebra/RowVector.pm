71730acc666983dd46bbb9d18d2a3512e0879185
gabarnettjr <gabarnettjr@gmail.com>
2023-08-03 18:41:13 -0600

    Update RowVector.pm

--- LinearAlgebra/RowVector.pm
+++ LinearAlgebra/RowVector.pm
@@ -4,18 +4,26 @@ use strict;
 use warnings;
 
 sub new {
-    my $numElements = undef;
-    my $elements = undef;
-    my $self = [$numElements, $elements];
+    my $numVals = undef;
+    my $vals = undef;
+    my $self = [$numVals, $vals];
     bless $self;
     return $self ;
 }
 
-sub numElements {
+sub numVals {
     my $self = shift;
-    my $numElements = @{$self}[0];
-    return $numElements if defined $numElements;
-    my $elements = $self->elements();
-    return scalar @{$elements};
+    my $numVals = @{$self}[0];
+    return $numVals if defined $numVals;
+    my $vals = $self->vals();
+    return scalar @{$vals};
     die "Failed to get the number of elements.";
+}
+
+sub vals {
+    my $self = shift;
+    my $vals = @{$self}[1];
+    return $vals if defined $vals && ! (scalar @_);
+    return @{$vals}[shift] if defined $vals;
+    die "\nFailed to get the values of the row vector.\n";
 }
