e4944497d64517c9663f149e577493d4422588fd
gabarnettjr <gabarnettjr@gmail.com>
2023-08-05 17:52:22 -0600

    Update Matrix.pm

--- LinearAlgebra/Matrix.pm
+++ LinearAlgebra/Matrix.pm
@@ -5,36 +5,20 @@ use warnings;
 
 
 sub new {
+    my $rows = shift;
+    
     my $numRows = undef;
     my $numCols = undef;
-    my $rows = undef;
-    my $self = [$numRows, $numCols, $rows];
+    
+    my $self = [$rows, $numRows, $numCols];
     bless(self);
     return $self;
 }
 
 
-sub numRows {
-    my $self = shift;
-    my $numRows = @{$self}[0];
-    return $numRows if defined $numRows;
-    return scalar @{$self->rows()};
-    die "\nFailed to get the number of rows.\n";
-}
-
-
-sub numCols {
-    my $self = shift;
-    my $numCols = @{$self}[1];
-    return $numCols if defined $numCols;
-    return scalar @{@{$self->rows()}[0]};
-    die "\nFailed to get the number of columns.\n";
-}
-
-
 sub rows {
     my $self = shift;
-    my $rows = @{$self}[2];
+    my $rows = @{$self}[0];
     return $rows if defined $rows && ! scalar @_;
     if (defined $rows) {
         my @out = ();
@@ -44,4 +28,23 @@ sub rows {
     }
     return \@out;
     die "\nFailed to get the row(s) of the matrix.\n";
+}
+
+
+
+sub numRows {
+    my $self = shift;
+    my $numRows = @{$self}[1];
+    return $numRows if defined $numRows;
+    return scalar @{$self->rows()};
+    die "\nFailed to get the number of rows.\n";
+}
+
+
+sub numCols {
+    my $self = shift;
+    my $numCols = @{$self}[2];
+    return $numCols if defined $numCols;
+    return scalar @{@{$self->rows()}[0]};
+    die "\nFailed to get the number of columns.\n";
 }
