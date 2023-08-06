b4ae973ca43c34addf7361bf0fbf4e1b8b2381cf
gabarnettjr <gabarnettjr@gmail.com>
2023-08-06 13:22:08 -0600

    Update Matrix.pm

--- LinearAlgebra/Matrix.pm
+++ LinearAlgebra/Matrix.pm
@@ -6,40 +6,58 @@ use strict;
 use warnings;
 
 
+
 sub new {
-    my $rows = shift;
+    my ($items, $numRows, $numCols);
+    if (scalar @_ == 1 && ref $_[0]) {
+        $items = shift;
+        $numRows = scalar @{$items};
+        $numCols = scalar @{@{$items}[0]};
+    } elsif (scalar @_ == 2 && ! ref $_[0] && ! ref $_[1]) {
+        $numRows = shift;
+        $numCols = shift;
+        my @items = ();
+        for (my $i = 0; $i < $numRows; $i++) {            
+            my @tmp = ();
+            for (my $j = 0; $j < $numCols; $j++) {
+                push(@tmp, 0);
+            }
+            push(@items, \@tmp);
+        }
+        $items = \@items;
+    } else {
+        die "\nBad input for new matrix.  Give ref to array or dimensions.\n";
+    }
     
-    my $numRows = undef;
-    my $numCols = undef;
-    
-    my $self = [$rows, $numRows, $numCols];
+    my $self = [$items, $numRows, $numCols];
     bless $self;
     return $self;
 }
 
 
-sub rows {
+
+sub items {
     my $self = shift;
-    my $rows = @{$self}[0];
-    return $rows if defined $rows && ! scalar @_;
+    my $items = @{$self}[0];
+    return $items if defined $items && ! scalar @_;
     die "\nFailed to get the row(s) of the matrix.\n";
 }
+
 
 
 sub numRows {
     my $self = shift;
     my $numRows = @{$self}[1];
     return $numRows if defined $numRows;
-    return scalar @{$self->rows()};
     die "\nFailed to get the number of rows.\n";
 }
+
 
 
 sub numCols {
     my $self = shift;
     my $numCols = @{$self}[2];
     return $numCols if defined $numCols;
-    return scalar @{@{$self->rows()}[0]};
     die "\nFailed to get the number of columns.\n";
 }
 
@@ -47,13 +65,14 @@ sub numCols {
 
 sub disp {
     my $self = shift;
-    foreach my $row (@{$self->rows()}) {
+    foreach my $row (@{$self->items()}) {
         foreach my $item (@{$row}) {
             print "$item ";
         }
         print "\n";
     }
 }
+
 
 
 sub item {
@@ -63,13 +82,24 @@ sub item {
     my $j = shift;
     
     if ($self->numRows() != 1 && $self->numCols() != 1 || (defined $i) && (defined $j)) {
-        return @{@{$self->rows()}[$i]}[$j];
+        return @{@{$self->items()}[$i]}[$j];
     } elsif ($self->numRows() == 1) {
-        return @{@{$self->rows()}[0]}[$i];
+        return @{@{$self->items()}[0]}[$i];
     } elsif ($self->numCols() == 1) {
-        return @{@{$self->rows()}[$i]}[0];
+        return @{@{$self->items()}[$i]}[0];
     }
 }
+
+
+
+sub set {
+    my $self = shift;
+    my $i = shift;
+    my $j = shift;
+    my $val = shift;
+    @{@{$self->items()}[$i]}[$j] = $val;
+}
+
 
 
 sub add {
@@ -78,22 +108,26 @@ sub add {
     
     my $numRows = $self->numRows();
     my $numCols = $self->numCols();
-    if ($numRows != $other->numRows() || $numCols != $other->numCols()) {
+    
+    if (ref $other && ($numRows != $other->numRows() || $numCols != $other->numCols())) {
         die "\nMatrices must be the same size to add them together.\n";
     }
     
-    my @out = ();
+    my $sum = Matrix::new($numRows, $numCols);
+    
     for (my $i = 0; $i < $numRows; $i++) {
-        my @tmp = ();
         for (my $j = 0; $j < $numCols; $j++) {
-            push(@tmp, $self->item($i, $j) + $other->item($i, $j));
+            if (ref $other) {
+                $sum->set($i, $j, $self->item($i, $j) + $other->item($i, $j));
+            } else {
+                $sum->set($i, $j, $self->item($i, $j) + $other);
+            }
         }
-        push(@out, \@tmp);
     }
     
-    my $sum = Matrix::new(\@out);
     return $sum;
 }
+
 
 
 sub dot {
@@ -105,6 +139,7 @@ sub dot {
     
     my $numRows = $self->numRows();
     my $numCols = $self->numCols();
+    
     if ($numRows != $other->numRows() || $numCols != $other->numCols()) {
         die "\nArrays must be the same size to dot them.\n";
     } elsif ($numRows != 1 && $numCols != 1) {
@@ -121,6 +156,13 @@ sub dot {
     
     return $dot;
 }
+
+
+
+sub transpose {
+    my $self = shift;
+}
+
 
 
 sub mult {
@@ -147,5 +189,5 @@ sub mult {
 }
 
 
-return 1;
 
+return 1;
