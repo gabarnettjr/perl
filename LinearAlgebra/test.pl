deaf731fe1241065fd15988339543c03c67a89e2
gabarnettjr <gabarnettjr@gmail.com>
2023-08-06 13:22:25 -0600

    Update test.pl

--- LinearAlgebra/test.pl
+++ LinearAlgebra/test.pl
@@ -8,8 +8,24 @@ use Matrix;
 
 print "\n";
 
-my $A = Matrix::new([[1,2,3], [4,5,6], [7,8,9]]);
+# my $A = Matrix::new([[1,2,3], [4,5,6], [7,8,9]]);
+my $A = Matrix::new(3, 3);
+for (my $i = 0; $i < $A->numRows(); $i++) {
+    for (my $j = 0; $j < $A->numCols(); $j++) {
+        $A->set($i, $j, $i + $j);
+    }
+}
+print "\$A = \n";
+$A->disp();
+
+print "\n";
+
 my $B = Matrix::new([[9,8,7], [6,5,4], [3,2,1]]);
+print "\$B = \n";
+$B->disp();
+
+print "\n";
+
 my $C = $A->add($B);
 print "\$A + \$B = \n";
 $C->disp();
