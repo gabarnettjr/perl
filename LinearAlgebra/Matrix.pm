88be059b5ab025276f54dfbc782ef90eeef23228
gabarnettjr <gabarnettjr@gmail.com>
2023-08-05 19:14:08 -0600

    Update Matrix.pm

--- LinearAlgebra/Matrix.pm
+++ LinearAlgebra/Matrix.pm
@@ -22,13 +22,6 @@ sub rows {
     my $self = shift;
     my $rows = @{$self}[0];
     return $rows if defined $rows && ! scalar @_;
-    if (defined $rows) {
-        my @out = ();
-        while (scalar @_) {
-            push @out, @{$rows}[shift @_];
-        }
-        return \@out;
-    }
     die "\nFailed to get the row(s) of the matrix.\n";
 }
 
@@ -48,6 +41,58 @@ sub numCols {
     return $numCols if defined $numCols;
     return scalar @{@{$self->rows()}[0]};
     die "\nFailed to get the number of columns.\n";
+}
+
+################################################################################
+
+sub disp {
+    my $self = shift;
+    foreach my $row (@{$self->rows()}) {
+        foreach my $item (@{$row}) {
+            print "$item ";
+        }
+        print "\n";
+    }
+}
+
+
+sub item {
+    my $self = shift;
+    
+    my $i = shift;
+    my $j = shift;
+    
+    if ($self->numRows() != 1 && $self->numCols() != 1 || (defined $i) && (defined $j)) {
+        return @{@{$self->rows()}[$i]}[$j];
+    } elsif ($self->numRows() == 1) {
+        return @{@{$self->rows()}[0]}[$i];
+    } elsif ($self->numCols() == 1) {
+        return @{@{$self->rows()}[$i]}[0];
+    }
+}
+
+
+sub add {
+    my $self = shift;
+    my $other = shift;
+    
+    my $numRows = $self->numRows();
+    my $numCols = $self->numCols();
+    if ($numRows != $other->numRows() || $numCols != $other->numCols()) {
+        die "\nMatrices must be the same size to add them together.\n";
+    }
+    
+    my @out = ();
+    for (my $i = 0; $i < $numRows; $i++) {
+        my @tmp = ();
+        for (my $j = 0; $j < $numCols; $j++) {
+            push(@tmp, $self->item($i, $j) + $other->item($i, $j));
+        }
+        push(@out, \@tmp);
+    }
+    
+    my $sum = Matrix::new(\@out);
+    return $sum;
 }
 
 
