98538bd51210f3ed6fbe0840a89d4f5cf70e8040
gabarnettjr <gabarnettjr@gmail.com>
2023-08-06 16:06:04 -0600

    Update Matrix.pm

--- LinearAlgebra/Matrix.pm
+++ LinearAlgebra/Matrix.pm
@@ -71,7 +71,7 @@ sub disp {
     my $self = shift;
     foreach my $row (@{$self->items()}) {
         foreach my $item (@{$row}) {
-            printf "%3i ", $item;
+            printf "%10.6f ", $item;
         }
         print "\n";
     }
@@ -109,11 +109,42 @@ sub row {
 
 sub set {
     my $self = shift;
-    my $i = shift;
-    my $j = shift;
-    my $val = shift;
     
-    @{@{$self->items()}[$i]}[$j] = $val;
+    my ($i, $j, $val);
+    
+    if (scalar @_ == 3) {
+        $i = shift;
+        $j = shift;
+        $val = shift;
+        @{@{$self->items()}[$i]}[$j] = $val;
+    } elsif (scalar @_ == 2 && $self->numRows() == 1) {
+        $j = shift;
+        $val = shift;
+        @{@{$self->items()}[0]}[$j] = $val;
+    } elsif (scalar @_ == 2 && $self->numCols() == 1) {
+        $i = shift;
+        $val = shift;
+        @{@{$self->items()}[$i]}[0] = $val;
+    } else {
+        print "\nInputs not understood.\n";
+        die;
+    }
+}
+
+################################################################################
+
+sub copy {
+    my $self = shift;
+    
+    my $out = Matrix::new($self->numRows(), $self->numCols());
+    
+    for (my $i = 0; $i < $out->numRows(); $i++) {
+        for (my $j = 0; $j < $out->numCols; $j++) {
+            $out->set($i, $j, $self->item($i, $j));
+        }
+    }
+    
+    return $out;
 }
 
 ################################################################################
@@ -202,6 +233,7 @@ sub times {
     my $prod;
     
     if (ref $other) {
+        
         if ($self->numCols() != $other->numRows()) {
             print "\n\$numCols of first matrix (" . $self->numCols() . ") must equal \$numRows of second (" . $other->numRows() . ").\n";
             print "\$first = \n";
@@ -211,14 +243,18 @@ sub times {
             $other->disp();
             die;
         }
+        
         $prod = Matrix::new($self->numRows(), $other->numCols());
         $other = $other->transpose();
+        
         for (my $i = 0; $i < $prod->numRows(); $i++) {
             for (my $j = 0; $j < $prod->numCols(); $j++) {
                 $prod->set($i, $j, $self->row($i)->dot($other->row($j)));
             }
         }
+        
     } else {
+        
         $prod = Matrix::new($self->numRows(), $self->numCols());
         for (my $i = 0; $i < $self->numRows(); $i++) {
             for (my $j = 0; $j < $self->numCols(); $j++) {
@@ -239,7 +275,10 @@ sub dotTimes {
     my $numRows = $self->numRows();
     my $numCols = $self->numCols();
     
-    if ($numRows != $other->numRows() || $numCols != $other->numCols()) {
+    if (! ref $other) {
+        print "\nUse times() to multiply a scalar by a matrix.\n";
+        die;
+    } elsif ($numRows != $other->numRows() || $numCols != $other->numCols()) {
         print "\nMatrices must be the same size to (dot) multiply them together.\n";
         die;
     }
@@ -253,6 +292,78 @@ sub dotTimes {
     }
     
     return $prod;
+}
+
+################################################################################
+
+sub swapRows {
+    my $self = shift;
+    my $i = shift;
+    my $j = shift;
+    
+    my $tmp = @{$self->items()}[$i];
+    @{$self->items()}[$i] = @{$self->items()}[$j];
+    @{$self->items()}[$j] = $tmp;
+}
+
+################################################################################
+
+sub solve {
+    my $self = shift;
+    my $rhs = shift;
+    
+    my $A = $self->copy();
+    my $b = $rhs->copy();
+
+    if ($A->numRows() != $A->numCols() || $A->numRows() != $b->numRows()) {
+        print "\nA should be square.  Rows(A) should equal len(b).\n";
+        die;
+    }
+    
+    my $nRows = $A->numRows();
+    my $nCols = $A->numCols();
+
+    # Apply row operations to transform A to upper triangular form.
+    for (my $j = 0; $j < $nCols - 1; $j++) {
+        # Find largest element in leftmost column, and make this the pivot row.
+        # Apparently, this is important, because when I was not doing this, I
+        # was getting noticeably different answers from python when the number
+        # of subdomains was small.
+        my $indMax = $j;
+        for (my $i = $j+1; $i < $nRows; $i++) {
+            if ((abs $A->item($i, $j)) > (abs $A->item($indMax, $j))) {
+                $indMax = $i;
+            }
+        }
+        # Swap row $indmax and row $j.
+        $A->swapRows($indMax, $j);
+        $b->swapRows($indMax, $j);
+        # my $tmp = $b->item($j);
+        # $b->set($j, $b->item($indMax));
+        # $b->set($indMax, $tmp);
+        # Zero out the $jth column.
+        for (my $i = $j + 1; $i < $nRows; $i++) {
+            my $factor = -1 * $A->item($i, $j) / $A->item($j, $j);
+            for (my $k = $j; $k < $nCols; $k++) {
+                $A->set($i, $k, $A->item($i, $k) + $factor * $A->item($j, $k));
+            }
+            $b->set($i, $b->item($i) + ($factor * $b->item($j)));
+        }
+    }
+
+    # Use back substitution to finish solving for @x.
+    my $x = $b;
+    $x->set($nRows-1, $b->item($nRows-1) / $A->item($nRows-1, $nRows-1));
+    my $k = $nRows;
+    for (my $i = $nRows - 2; $i >= 0; $i--) {
+        $k--;
+        my $dot = 0;
+        for (my $j = $k; $j < $nRows; $j++) {
+            $dot += ($A->item($i, $j) * $x->item($j));
+        }
+        $x->set($i, ($b->item($i) - $dot) / $A->item($i, $i));
+    }
+    return $x;
 }
 
 ################################################################################
