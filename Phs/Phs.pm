edee96eba3921410d4f4ed919e4960c9bd83060b
gabarnettjr <gabarnettjr@gmail.com>
2023-08-13 13:40:47 -0600

    Update Phs.pm

--- Phs/Phs.pm
+++ Phs/Phs.pm
@@ -6,7 +6,16 @@ use strict;
 use warnings;
 
 sub new {
-
+    my $dims = shift;
+    my $exponent = shift;
+    my $nodes = shift;
+    
+    my $evalPts = undef;
+    my $coeffs = undef;
+    
+    my $self = [$dims, $exponent, $nodes, $evalPts, $coeffs];
+    bless $self;
+    return $self;
 }
 
 return 1;
