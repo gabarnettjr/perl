97285fe31d880c03e304502c07ab06242f59570f
gabarnettjr <gabarnettjr@gmail.com>
2023-08-13 15:53:45 -0600

    Update test.pl

--- Phs/test.pl
+++ Phs/test.pl
@@ -6,4 +6,18 @@ use warnings;
 use lib ".";
 use Phs;
 
-my $phs = Phs::new();
+use lib "../LinearAlgebra";
+use Matrix;
+
+my $n = 6;
+my $N = 19;
+
+my $x = Matrix::linspace(-1, 1, $n);
+my $y = Matrix::linspace(-1, 1, $n);
+my ($xx, $yy) = Matrix::meshgrid($x, $y);
+
+my $X = Matrix::linspace(-1, 1, $N);
+my $Y = Matrix::linspace(-1, 1, $N);
+my ($XX, $YY) = Matrix::meshgrid($X, $Y);
+
+my $phs = Phs::new(2, 5, $nodes);
