65513d78625047f5cc90ede28552e52547ca8320
gabarnettjr <gabarnettjr@gmail.com>
2023-08-13 13:49:28 -0600

    Update Phs.pm

--- Phs/Phs.pm
+++ Phs/Phs.pm
@@ -5,6 +5,8 @@ package Phs;
 use strict;
 use warnings;
 
+
+
 sub new {
     my $dims = shift;
     my $exponent = shift;
@@ -17,5 +19,34 @@ sub new {
     bless $self;
     return $self;
 }
+
+
+
+sub dims {
+    my $self = shift;
+    my $dims = @{$self}[0];
+    return $dims if defined $dims;
+    print STDERR "\nFailed to get dimensions.\n";  die;
+}
+
+
+
+sub exponent {
+    my $self = shift;
+    my $exponent = @{$self}[1];
+    return $exponent if defined $exponent;
+    print STDERR "\nFailed to get exponent.\n";  die;
+}
+
+
+
+sub $nodes {
+    my $self = shift;
+    my $nodes = @{$self}[2];
+    return $nodes if defined $nodes && ! scalar @_;
+    return $nodes[shift] if defined $nodes;
+}
+
+
 
 return 1;
