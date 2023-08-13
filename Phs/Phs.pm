15c44bf64d258d643f84a15038954ed4829c90b0
gabarnettjr <gabarnettjr@gmail.com>
2023-08-13 13:52:53 -0600

    Update Phs.pm

--- Phs/Phs.pm
+++ Phs/Phs.pm
@@ -45,6 +45,17 @@ sub $nodes {
     my $nodes = @{$self}[2];
     return $nodes if defined $nodes && ! scalar @_;
     return $nodes[shift] if defined $nodes;
+    print STDERR "\nFailed to get the nodes.\n";  die;
+}
+
+
+
+sub $evalPts {
+    my $self = shift;
+    my $evalPts = @{$self}[3];
+    return $evalPts if defined $evalPts && ! scalar @_;
+    return $evalPts[shift] if defined $evalPts;
+    print STDERR "\nFailed to get the evaluation points.\n";  die;
 }
 
 
