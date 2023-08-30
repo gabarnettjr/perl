9528f19ad48107f6f2fa2244c2e61ad45c49119e
gabarnettjr <gabarnettjr@gmail.com>
2023-08-29 18:40:40 -0600

    Create testLocalPhs.pl

---
+++ Phs/testLocalPhs.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib ".";
+use Phs;
+
+use lib "../LinearAlgebra";
+use Matrix;
+
+my $rbfExponent = 3;
+my $polyDegree = 1;
+
+# Define spatial domain [a,b] x [c,d], with m rows and n columns.
+my $a = -1;  my $b = 1;  my $n = 4;
+my $c = -1;  my $d = 1;  my $m = 4;
+
+# Define number of rows (M) and columns (N) for evaluation points.
+my $M = 7;
+my $N = 7;
+
+################################################################################
+
+my ($x, $y, $xx, $yy, $nodes, $zz);
+eval {
+    $x = Matrix::linspace($a, $b, $n);
+    $y = Matrix::linspace($c, $d, $m);
+    ($xx, $yy) = Matrix::meshgrid($x, $y);
+    $xx = $xx->flatten();  $yy = $yy->flatten();
+    $nodes = $xx->vstack($yy);
+    $zz = Phs::testFunc2d($nodes);
+};
+die if $@;
+
+my ($X, $Y, $XX, $YY, $NODES, $ZZ);
+eval {
+    $X = Matrix::linspace($a, $b, $N);
+    $Y = Matrix::linspace($c, $d, $M);
+    ($XX, $YY) = Matrix::meshgrid($X, $Y);
+    $XX = $XX->flatten();  $YY = $YY->flatten();
+    $NODES = $XX->vstack($YY);
+    $ZZ = Phs::testFunc2d($NODES);
+};
+die if $@;
+
+my ($phs, $estimate);
+eval {
+    $phs = Phs::new($rbfExponent, $polyDegree, $nodes, $zz);
+    $estimate = $phs->evaluate($NODES);
+    # $phs->coeffs()->disp();
+};
+die if $@;
+
+my $diff;
+eval {
+    print "\$estimate = \n";
+    $estimate->disp();
+    print "\n";
+    print "\$ZZ = \n";
+    $ZZ->disp();
+    print "\n";
+    $diff = $estimate->plus($ZZ->dot(-1));
+    print "\$diff = \n";
+    $diff->disp();
+};
+die if $@;
