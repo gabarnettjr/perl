bef7cc07505ae0f67960c9d4f7feec787f1f0c36
gabarnettjr <gabarnettjr@gmail.com>
2023-08-29 18:17:24 -0600

    Update Subdomain.pm

--- Phs/Subdomain.pm
+++ Phs/Subdomain.pm
@@ -5,7 +5,7 @@ package Subdomain;
 use strict;
 use warnings;
 
-use lib "../LinearAlgebra";
+use lib ".";
 use Matrix;
 
 ################################################################################
